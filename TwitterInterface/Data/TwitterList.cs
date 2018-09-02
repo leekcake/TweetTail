b8f28f6496ff3ab72f32023078a98e6c344e8b63
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-02 16:45:22 +0900

    Add TwitterList

---
+++ TwitterInterface/Data/TwitterList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterInterface.Data
+{
+    class TwitterList
+    {
+        public long id;
+
+        public string slut;
+
+        public string name;
+        public string fullName;
+
+        public string description;
+
+        public DateTime createdAt;
+        public string url;
+
+        public long subscriberCount;
+        public long memberCount;
+        public string mode;
+
+        public User user;
+        public bool following;
+    }
+}
