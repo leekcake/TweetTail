8022d7a27fd3cbcb01c0d1328d17e4d26ee798e5
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-03 20:32:50 +0900

    Add Friendship

---
+++ TwitterInterface/Data/Friendship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterInterface.Data
+{
+    class Friendship
+    {
+        public long id;
+
+        public string name;
+        public string screenName;
+
+        public bool isFollowing;
+        public bool isFollowingRequested;
+        public bool isFollowedBy;
+        public bool isBlocking;
+        public bool isMuting;
+    }
+}
