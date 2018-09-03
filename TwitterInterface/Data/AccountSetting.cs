6a0eb3671374cdf0f560df3a39c4970490ba7b22
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-03 21:41:18 +0900

    Add AccountSetting

---
+++ TwitterInterface/Data/AccountSetting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterInterface.Data
+{
+    class AccountSetting
+    {
+        public class SleepTime
+        {
+            public bool isEnabled;
+            //TODO: Replace with (custom) class
+            public string startTime, endTime;
+        }
+
+        public class TrendLocation
+        {
+            public string country;
+            public string countryCode;
+            public string name;
+
+            public long parentId;
+
+            //placeType {
+            public long placeTypeCode;
+            public string placeTypeName;
+            //}
+
+            public string url;
+            public string woeid;
+        }
+
+        public bool isAlwaysUseHttps;
+        public bool isDiscoverableByEmail;
+        public bool isGeoEnabled;
+
+        public string language;
+
+        public bool isProtected;
+
+        public string screenName;
+
+        public bool showAllInlineMedia;
+
+        public SleepTime sleepTime;
+        public TimeZoneInfo timeZone;
+        public TrendLocation trendLocation;
+
+        public bool useCookiePersonalization;
+        public string allowContributorRequest;
+    }
+}
