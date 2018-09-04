beb7a1517e34f8cabf13f5ed4224d9ec3dbc9d80
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-04 21:06:25 +0900

    Add AccountAPI

---
+++ TwitterInterface/API/AccountAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TwitterInterface.Data;
+
+namespace TwitterInterface.API
+{
+    interface AccountAPI
+    {
+        //GET account/settings
+        AccountSetting GetAccountSetting(Account account);
+        //GET account/verify_credentials
+        User VerifyCredentials(Account account, bool includeEntities = true, bool skipStatus = false, bool includeEmail = false);
+        //GET users/profile_banner
+        string[] GetBannerImageVariant(Account account, long userId);
+        string[] GetBannerImageVariant(Account account, string screenName);
+
+        //POST account/remove_profile_banner
+        void RemoveProfileBanner(Account account);
+        //TODO: POST account/settings
+
+        //POST account/update_profile
+        User UpdateProfile(Account account, string name, string url, string location, string description, string profileLinkColor, bool includeEntities = true, bool skipStatus = false);
+        //POST account/update_profile_banner
+        void UpdateProfileBanner(Account account, Stream image);
+        //POST account/update_profile_image
+        User UpdateProfileImage(Account account, Stream image, bool includeEntities = true, bool skipStatus = false);
+
+
+        //GET saved_searches/list
+        List<SavedSearch> GetSavedSearches(Account account);
+        //GET saved_searches/show/:id
+        SavedSearch GetSavedSearchById(Account account, long id);
+
+        //POST saved_searches/create
+        SavedSearch CreateSavedSearch(Account account, string query);
+        //POST saved_searches/destroy/:id
+        SavedSearch DestroySavedSearch(Account account, long id);
+    }
+}
