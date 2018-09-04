ce9e10a64a02e5f023e7ece18c74d5c6e844f74b
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-04 20:27:53 +0900

    Add SavedSearch

---
+++ TwitterInterface/Data/SavedSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterInterface.Data
+{
+    class SavedSearch
+    {
+        public long id;
+
+        public DateTime createdAt;
+        public string name;
+        public string query;
+        
+        //TODO: position
+    }
+}
