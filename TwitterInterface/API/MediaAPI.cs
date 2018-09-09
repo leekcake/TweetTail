dc62e55278791b3f136caf5d7e7b893d19186512
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-09 19:51:13 +0900

    Add MediaAPI

---
+++ TwitterInterface/API/MediaAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using TwitterInterface.Data;
+
+namespace TwitterInterface.API
+{
+    interface MediaAPI
+    {
+        //POST media/upload
+        long uploadMedia(Account account, Stream image);
+    }
+}
