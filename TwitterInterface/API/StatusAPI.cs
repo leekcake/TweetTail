a052a4b6426b3734fb57045e4c350bb088bad15c
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-09 12:43:06 +0900

    Add more functions to StatusAPI

--- TwitterInterface/API/StatusAPI.cs
+++ TwitterInterface/API/StatusAPI.cs
@@ -13,7 +13,44 @@ namespace TwitterInterface.API
         //GET statuses/user_timeline
         List<Status> GetUserline(Account account, long userId, long count = 200, long sinceId = -1, long maxId = -1);
 
+        //GET statuses/retweets_of_me
+        List<Status> GetAccountRetweets(Account account, long count = 200, long sinceId = -1, long maxId = -1);
+
         //GET statuses/mentions_timeline
         List<Status> GetMentionline(Account account, long count = 200, long sinceId = -1, long maxId = -1);
+
+        //POST statuses/update
+        Status CreateStatus(Account account, StatusUpdate update);
+
+        //POST statuses/destroy/:id
+        Status DestroyStatus(Account account, long id);
+
+        //GET statuses/show/:id
+        Status GetStatuses(Account account, long id);
+        //TODO: GET statuses/oembed
+        //GET statuses/lookup
+        List<Status> GetStatuses(Account account, long[] ids);
+
+        //POST statuses/retweet/:id
+        Status RetweetStatus(Account account, long id);
+
+        //POST statuses/unretweet/:id
+        Status UnretweetStatus(Account account, long id);
+
+        //GET statuses/retweets/:id
+        List<Status> GetRetweetedStatus(Account account, long id, long count = 100);
+
+        //GET statuses/retweeters/ids
+        CursoredList<long> GetRetweeterIds(Account account, long id, long count = 100, long cursor = -1);
+
+        //POST favorites/create
+        Status CreateFavorite(Account account, long id);
+
+        //POST favorites/destroy
+        Status DestroyFavorite(Account account, long id);
+
+        //GET favorites/list
+        List<Status> GetFavorites(Account account, long userId, long count = 200, long sinceId = -1, long maxId = -1);
+        List<Status> GetFavorites(Account account, string userScreenName, long count = 200, long sinceId = -1, long maxId = -1);
     }
 }
---
+++ TwitterInterface/Data/StatusUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterInterface.Data
+{
+    class StatusUpdate
+    {
+        string text;
+
+        long inReplyToStatusId;
+
+        bool autoPopulateReplyMetadata = false;
+        long[] excludeReplyUserIds;
+
+        string attachmentURL;
+
+        long[] mediaIDs;
+
+        bool possiblySensitive;
+        
+        //TODO: lat
+        //TODO: long
+        //TODO: place
+    }
+}
