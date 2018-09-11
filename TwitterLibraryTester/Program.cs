3c3c820f4313557511f98ba8a0573e7013ada569
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-11 23:51:23 +0900

    Add TwitterLibraryTester project

---
+++ TwitterLibraryTester/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterInterface.Data;
+using TwitterLibrary;
+
+namespace TwitterLibraryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new APIImpl();
+            LoginTest(api);
+
+            System.Console.ReadLine();
+        }
+
+        static async void LoginTest(APIImpl api)
+        {
+            var consumer = new Token(HiddenStore.sampleConsumerKey, HiddenStore.sampleConsumerSecret);
+
+            var token = await api.GetLoginTokenAsync(consumer);
+        }
+    }
+}
