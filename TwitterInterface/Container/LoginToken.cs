d7ea833ae98a70c18a52f1439e2f8e3828175483
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-11 19:58:03 +0900

    Add LoginToken

---
+++ TwitterInterface/Container/LoginToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TwitterInterface.Data;
+
+namespace TwitterInterface.Container
+{
+    /// <summary>
+    /// Helper class for login to twitter
+    /// </summary>
+    public interface LoginToken
+    {
+        string loginURL {
+            get;
+        }
+
+        Account login(string pin);
+    }
+}
