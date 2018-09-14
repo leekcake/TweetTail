789b11ba9c187afc4a6d7ec797728b0e8dd5796e
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-14 21:01:58 +0900

    Add missing account parameter on CollectionAPI

--- TwitterInterface/API/CollectionAPI.cs
+++ TwitterInterface/API/CollectionAPI.cs
@@ -10,35 +10,35 @@ namespace TwitterInterface.API
     public interface CollectionAPI
     {
         //GET collections/entries
-        Task<GetEntriesResult> GetEntries(string id, long count, long maxPosition = -1, long minPosition = -1);
+        Task<GetEntriesResult> GetEntries(Account account, string id, long count, long maxPosition = -1, long minPosition = -1);
 
         //GET collections/list
-        Task<FindListResult> FindList(long userId, long count, string cursor);
-        Task<FindListResult> FindList(string screenName, long count, string cursor);
-        Task<FindListResult> FindListByTweetId(long tweetId, long count, string cursor);
+        Task<FindListResult> FindList(Account account, long userId, long count, string cursor);
+        Task<FindListResult> FindList(Account account, string screenName, long count, string cursor);
+        Task<FindListResult> FindListByTweetId(Account account, long tweetId, long count, string cursor);
 
         //GET collections/show
-        Task<Collection> GetCollection(string id);
+        Task<Collection> GetCollection(Account account, string id);
 
         //POST collections/create
-        Task<Collection> CreateCollection(string name, string description, string url, Collection.Order? order);
+        Task<Collection> CreateCollection(Account account, string name, string description, string url, Collection.Order? order);
 
         //POST collections/update
-        Task<Collection> UpdateCollection(string id, string name, string description, string url);
+        Task<Collection> UpdateCollection(Account account, string id, string name, string description, string url);
 
         //POST collections/destroy
-        Task DestroyCollection(string id);
+        Task DestroyCollection(Account account, string id);
 
         //POST collections/entries/add
-        Task AddTweetToCollection(string id, long statusId, long relativeTo, bool above = true);
+        Task AddTweetToCollection(Account account, string id, long statusId, long relativeTo, bool above = true);
         //POST collections/entries/move
-        Task MoveTweetFromCollection(string id, long[] statusId, long relativeTo, bool above = true);
+        Task MoveTweetFromCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true);
         //POST collections/entries/remove
-        Task RemoveTweetFromCollection(string id, long statusId);
+        Task RemoveTweetFromCollection(Account account, string id, long statusId);
 
         //POST collections/entries/curate
-        Task AddAllTweetToCollection(string id, long[] statusId, long relativeTo, bool above = true);
-        Task RemoveAllTweetFromCollection(string id, long[] statusId, long relativeTo, bool above = true);
+        Task AddAllTweetToCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true);
+        Task RemoveAllTweetFromCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true);
 
 
     }
