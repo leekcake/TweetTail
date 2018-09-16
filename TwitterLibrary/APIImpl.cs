01b81638305286a8a6f61b19aeeaa8443e41ff9d
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-16 14:37:53 +0900

    Impl CollectionAPI

--- TwitterLibrary/APIImpl.cs
+++ TwitterLibrary/APIImpl.cs
@@ -12,10 +12,11 @@ using TwitterInterface.Data;
 using TwitterLibrary.Data;
 using TwitterLibrary.Container;
 using Newtonsoft.Json.Linq;
+using TwitterInterface.API.Result;
 
 namespace TwitterLibrary
 {
-    public class APIImpl : AccountAPI
+    public class APIImpl : AccountAPI, CollectionAPI
     {
         private static KeyValuePair<string, string>[] makeQuery(params string[] query)
         {
@@ -204,6 +205,166 @@ namespace TwitterLibrary
             });
 
             return TwitterDataFactory.parseUser(JObject.Parse(response));
+        }
+
+        public async Task<GetEntriesResult> GetEntries(Account account, string id, long count, long maxPosition = -1, long minPosition = -1)
+        {
+            var response = await Get("https://api.twitter.com/1.1/collections/entries.json", account,
+                makeQuery("id", id, "count", count.ToString(),
+                "max_position", maxPosition != -1 ? maxPosition.ToString() : null,
+                "min_position", minPosition != -1 ? minPosition.ToString() : null));
+
+            var json = JObject.Parse(response);
+
+            var result = new GetEntriesResult();
+
+            var position = json["response"]["position"];
+            result.minPosition = position["min_position"].ToObject<long>();
+            result.maxPosition = position["max_position"].ToObject<long>();
+
+            result.collectionTweets = TwitterDataFactory.parseArray(json["response"]["timeline"].ToObject<JArray>(), TwitterDataFactory.parseCollectionTweet).ToList();
+            result.collection = TwitterDataFactory.parseCollection(json["objects"]["timelines"][
+                json["response"]["timeline-id"].ToString()
+                ].ToObject<JObject>());
+
+            result.tweet = new List<Status>();
+            foreach (var cTweet in result.collectionTweets)
+            {
+                result.tweet.Add(TwitterDataFactory.parseStatus(json["objects"]["tweets"][cTweet.tweetId.ToString()].ToObject<JObject>()));
+            }
+
+            return result;
+        }
+
+        private FindListResult FindList(string response)
+        {
+            var json = JObject.Parse(response);
+            var result = new FindListResult();
+
+            result.nextCursor = json["response"]["cursors"]["next_cursor"].ToString();
+            result.collections = new List<Collection>();
+
+            var results = json["response"]["result"].ToObject<JArray>();
+            foreach(var timeline in results)
+            {
+                result.collections.Add(TwitterDataFactory.parseCollection(json["objects"]["timelines"][timeline["timeline-id"].ToString()].ToObject<JObject>()));
+            }
+
+            return result;
+        }
+
+        public async Task<FindListResult> FindList(Account account, long userId, long count, string cursor)
+        {
+            return FindList(await Get("https://api.twitter.com/1.1/collections/list.json", account,
+                makeQuery("user_id", userId.ToString(), "count", count != -1 ? count.ToString() : null, "cursor", cursor)
+                ));
+        }
+
+        public async Task<FindListResult> FindList(Account account, string screenName, long count, string cursor)
+        {
+            return FindList(await Get("https://api.twitter.com/1.1/collections/list.json", account,
+                makeQuery("screen_name", screenName, "count", count != -1 ? count.ToString() : null, "cursor", cursor)
+                ));
+        }
+
+        public async Task<FindListResult> FindListByTweetId(Account account, long tweetId, long count, string cursor)
+        {
+            return FindList(await Get("https://api.twitter.com/1.1/collections/list.json", account,
+                makeQuery("tweet_id", tweetId.ToString(), "count", count != -1 ? count.ToString() : null, "cursor", cursor)
+                ));
+        }
+
+        public async Task<Collection> GetCollection(Account account, string id)
+        {
+            var json = JObject.Parse(await Get("https://api.twitter.com/1.1/collections/show.json", account,
+                makeQuery("id", id)));
+
+            return TwitterDataFactory.parseCollection(json["objects"]["timelines"][json["response"]["timeline_id"].ToString()].ToObject<JObject>());
+        }
+
+        public async Task<Collection> CreateCollection(Account account, string name, string description, string url, Collection.Order? order)
+        {
+            var json = JObject.Parse(await Post("https://api.twitter.com/1.1/collections/create.json", account,
+                makeQuery("name", name, "description", description, "url", url, "order", order != null ? Collection.OrderToString(order.Value) : null )));
+
+            return TwitterDataFactory.parseCollection(json["objects"]["timelines"][json["response"]["timeline_id"].ToString()].ToObject<JObject>());
+        }
+
+        public async Task<Collection> UpdateCollection(Account account, string id, string name, string description, string url)
+        {
+            var json = JObject.Parse(await Post("https://api.twitter.com/1.1/collections/update.json", account,
+                makeQuery("id", id, "name", name, "description", description, "url", url)));
+            //TODO: I think UpdateColllection's result collection data is incomplete?
+            return TwitterDataFactory.parseCollection(json["objects"]["timelines"][json["response"]["timeline_id"].ToString()].ToObject<JObject>());
+        }
+
+        public async Task DestroyCollection(Account account, string id)
+        {
+            //TODO: Error handleing
+            await Post("https://api.twitter.com/1.1/collections/destroy.json", account, makeQuery("id", id));
+        }
+
+        public async Task AddTweetToCollection(Account account, string id, long statusId, long relativeTo, bool above = true)
+        {
+            //TODO: Error handleing
+            await Post("https://api.twitter.com/1.1/collections/entries/add.json", account,
+                makeQuery("id", id, "tweet_id", statusId.ToString(), "relative_to", relativeTo != -1 ? relativeTo.ToString() : null, 
+                "above", above == false ? above.ToString() : null));
+        }
+
+        public async Task MoveTweetFromCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true)
+        {
+            //TODO: Error handleing
+            await Post("https://api.twitter.com/1.1/collections/entries/move.json", account,
+                makeQuery("id", id, "tweet_id", statusId.ToString(), "relative_to", relativeTo != -1 ? relativeTo.ToString() : null,
+                "above", above == false ? above.ToString() : null));
+        }
+
+        public async Task RemoveTweetFromCollection(Account account, string id, long statusId)
+        {
+            //TODO: Error handleing
+            await Post("https://api.twitter.com/1.1/collections/entries/remove.json", account,
+                makeQuery("id", id, "tweet_id", statusId.ToString()));
+        }
+
+        private JObject makeCurateJson(string op, string id, long[] statusId)
+        {
+            var json = new JObject();
+            json["id"] = id;
+            var changes = new JArray();
+
+            foreach (var sid in statusId)
+            {
+                var operationJson = new JObject();
+                operationJson["op"] = op;
+                operationJson["tweet_id"] = sid;
+                changes.Add(operationJson);
+            }
+
+            json["changes"] = changes;
+
+            return json;
+        }
+
+        private async Task handleCurate(HttpRequestMessage message, JObject operation)
+        {
+            message.Content = new StringContent(operation.ToString(), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(message);
+        }
+
+        public async Task AddAllTweetToCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true)
+        {
+            //TODO: Error handleing
+            await handleCurate(Utils.generateHttpRequest(HttpMethod.Post, new Uri("https://api.twitter.com/1.1/collections/entries/curate.json"), new KeyValuePair<string, string>[] { }
+            , account as LibAccount), makeCurateJson("add", id, statusId));
+        }
+
+        public async Task RemoveAllTweetFromCollection(Account account, string id, long[] statusId, long relativeTo, bool above = true)
+        {
+            //TODO: Error handleing
+            await handleCurate(Utils.generateHttpRequest(HttpMethod.Post, new Uri("https://api.twitter.com/1.1/collections/entries/curate.json"), new KeyValuePair<string, string>[] { }
+            , account as LibAccount), makeCurateJson("remove", id, statusId));
         }
     }
 }
