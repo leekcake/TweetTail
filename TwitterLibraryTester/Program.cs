2c71f1abea49c9c5ffc2a52ce716ee7a3592b583
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-16 20:17:41 +0900

    Add ReadTweetTest / PostTweetTest

--- TwitterLibraryTester/Program.cs
+++ TwitterLibraryTester/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using TwitterInterface.Data;
 using TwitterLibrary;
 using System.IO;
+using System.Runtime.ExceptionServices;
 
 namespace TwitterLibraryTester
 {
@@ -24,12 +25,39 @@ namespace TwitterLibraryTester
 
         static void Main(string[] args)
         {
-            var api = new APIImpl();
-            LoginTest(api).Wait();
-            VerifyTest(api).Wait();
+            var task = TestPerform();
+
+            while (!task.IsCompleted && !task.IsCanceled && !task.IsFaulted)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            ThrowError(task);
 
             Console.WriteLine("End of Test");
             Console.ReadLine();
+        }
+
+        private static void ThrowError(Task t)
+        {
+            if (t.IsFaulted)
+            {
+                Exception exception =
+                    t.Exception.InnerExceptions != null && t.Exception.InnerExceptions.Count == 1
+                        ? t.Exception.InnerExceptions[0]
+                        : t.Exception;
+
+                ExceptionDispatchInfo.Capture(exception).Throw();
+            }
+        }
+
+        static async Task TestPerform()
+        {
+            var api = new APIImpl();
+
+            await LoginTest(api);
+            await VerifyTest(api);
+            await ReadTweetTest(api);
+            await PostTweetTest(api);
         }
 
         static async Task LoginTest(APIImpl api)
@@ -74,6 +102,29 @@ namespace TwitterLibraryTester
 
 
             Console.WriteLine("Verify Test OK");
+        }
+
+        static async Task ReadTweetTest(APIImpl api)
+        {
+            Console.WriteLine("ReadTweetTest Test");
+
+            var line = await api.GetTimeline(account);
+
+            Console.WriteLine("Latest Timeline tweet text: " + line[0].text);
+
+            Console.WriteLine("ReadTweetTest Test OK");
+        }
+
+        static async Task PostTweetTest(APIImpl api)
+        {
+            Console.WriteLine("Post Tweet Test");
+
+            var update = new StatusUpdate();
+            update.text = "샘플 데-스!";
+
+            var status = await api.CreateStatus(account, update);
+
+            Console.WriteLine("Post Tweet Test OK");
         }
     }
 }
