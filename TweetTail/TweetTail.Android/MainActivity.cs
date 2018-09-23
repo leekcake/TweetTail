c672914c215fca1a832299d0eba1a84b5f5f2d40
Blue Electric <fkdlxmslrx@gmail.com>
2018-09-23 20:48:39 +0900

    Add Video/GIF playback support

--- TweetTail/TweetTail.Android/MainActivity.cs
+++ TweetTail/TweetTail.Android/MainActivity.cs
@@ -7,14 +7,42 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Plugin.CurrentActivity;
+using Android.Content;
+using System.Threading.Tasks;
 
 namespace TweetTail.Droid
 {
     [Activity(Label = "TweetTail", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        // Field, properties, and method for Video Picker
+        public static MainActivity Current { private set; get; }
+        public static readonly int PickImageId = 1000;
+
+        public TaskCompletionSource<string> PickImageTaskCompletionSource { set; get; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (data != null))
+                {
+                    // Set the filename as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(data.DataString);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Current = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
---
+++ TweetTail/TweetTail/Status/MediaView.cs
@@ -0,0 +1,67 @@
+﻿using FFImageLoading.Forms;
+using FormsVideoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterInterface.Data.Entity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DataStatus = TwitterInterface.Data.Status;
+
+namespace TweetTail.Status
+{
+    public partial class MediaPage : CarouselPage
+    {
+        private DataStatus viewing;
+
+        private VideoPlayer videoView;
+
+        public MediaPage(DataStatus status, int inx = 0)
+        {
+            viewing = status;
+
+            if (status.extendMedias[0].type == "photo")
+            {
+                foreach (var media in status.extendMedias)
+                {
+                    Children.Add(new ContentPage()
+                    {
+                        Content = new CachedImage()
+                        {
+                            Source = new UriImageSource()
+                            {
+                                Uri = new Uri(media.mediaURLHttps)
+                            }
+                        }
+                    });
+                }
+                CurrentPage = Children[inx];
+            }
+            else
+            {
+                videoView = new VideoPlayer();
+                Children.Add(new ContentPage()
+                {
+                    Content = videoView
+                });
+                videoView.Source = VideoSource.FromUri(pickVideoVariant(status.extendMedias[0].video.variants).url);
+            }
+        }
+
+        private VideoVariant pickVideoVariant(VideoVariant[] variants)
+        {
+            var best = variants[0];
+            foreach (var variant in variants)
+            {
+                if (best.bitrate < variant.bitrate)
+                {
+                    best = variant;
+                }
+            }
+            return best;
+        }
+    }
+}
