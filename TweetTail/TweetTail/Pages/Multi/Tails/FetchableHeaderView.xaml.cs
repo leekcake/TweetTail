08aa1f74c144b8a2b7875a03853ce0d84e7723e2
Blue Electric <fkdlxmslrx@gmail.com>
2018-10-26 23:53:16 +0900

    Add AutoRefresh to FetchableHeader

--- TweetTail/TweetTail/Pages/Multi/Tails/FetchableHeaderView.xaml.cs
+++ TweetTail/TweetTail/Pages/Multi/Tails/FetchableHeaderView.xaml.cs
@@ -12,6 +12,8 @@ namespace TweetTail.Pages.Multi.Tails
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FetchableHeaderView : ContentView
     {
+        public int RefreshRate = 7200; //큿
+        
         public delegate void RefreshRequest();
         public event RefreshRequest OnRefreshRequested;
 
@@ -29,6 +31,17 @@ namespace TweetTail.Pages.Multi.Tails
             set {
                 if (value == inRefresh) return;
                 inRefresh = value;
+
+                if (!inRefresh && AutoRefresh)
+                {
+                    new Task(async () =>
+                    {
+                        System.Diagnostics.Debug.WriteLine("Refresh requested");
+                        await Task.Delay(RefreshRate);
+                        DoRefresh();
+                    }).Start();
+                }
+
                 UpdateRefreshIcon();
                 return;
             }
@@ -39,22 +52,56 @@ namespace TweetTail.Pages.Multi.Tails
             RefreshIcon.Source = InRefresh ? "ic_repeat_green_300_24dp" : "ic_repeat_grey_500_24dp";
         }
 
+        private bool autoRefresh = true;
+        public bool AutoRefresh {
+            get {
+                return autoRefresh;
+            }
+            set {
+                if (value == autoRefresh) return;
+                autoRefresh = value;
+                UpdateAutoRefreshIcon();
+            }
+        }
+        private void UpdateAutoRefreshIcon()
+        {
+            AutoRefreshIcon.Source = AutoRefresh ? "ic_sync_green_300_24dp" : "ic_sync_disabled_grey_500_24dp";
+        }
+
         public FetchableHeaderView()
         {
             InitializeComponent();
+
             UpdateRefreshIcon();
+            UpdateAutoRefreshIcon();
+
+            if (AutoRefresh) DoRefresh();
+
             RefreshIcon.GestureRecognizers.Add(new TapGestureRecognizer()
             {
                 Command = new Command(() =>
                 {
-                    if(InRefresh)
-                    {
-                        return;
-                    }
-                    InRefresh = true;
-                    OnRefreshRequested?.Invoke();
+                    if(!AutoRefresh)
+                        DoRefresh();
                 })
             });
+        }
+
+        private void DoRefresh()
+        {
+            if (InRefresh)
+            {
+                return;
+            }
+            InRefresh = true;
+            if(OnRefreshRequested != null)
+            {
+                OnRefreshRequested.Invoke();
+            }
+            else
+            {
+                InRefresh = false;
+            }
         }
     }
 }
